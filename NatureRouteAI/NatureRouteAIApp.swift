51c806a0175036c668881e6521fa55031fd7a48a
Mykola <89003122+Kolya-Sudak-fr@users.noreply.github.com>
2026-03-12 12:24:23 +0100

    Initial Commit

---
+++ NatureRouteAI/NatureRouteAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureRouteAIApp.swift
+//  NatureRouteAI
+//
+//  Created by Kolya Sudak on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct NatureRouteAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
